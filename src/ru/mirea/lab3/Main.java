889f1fcd87b90b55974017fd2840e464f1fcbfdc
dmcirfan <danil.milshin.07.12@gmail.com>
2020-09-16 15:52:12 +0300

    Initial commit

---
+++ src/ru/mirea/lab3/Main.java
@@ -0,0 +1,26 @@
+package ru.mirea.lab3;
+
+public class Main {
+    public static void main(String[] args)
+{
+    var shapes = new Shape[10];
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (i % 2 == 0)
+        {
+            shapes[i] = new Circle(Math.random() * 100, "Black", i % 3 == 0);
+        }
+        else
+        {
+            shapes[i] = new Rectangle(Math.random() * 100, Math.random() * 100, "Black",  i % 3 == 0);
+        }
+    }
+
+    for (var current : shapes)
+    {
+        System.out.println(current.toString());
+        System.out.println();
+    }
+}
+}
